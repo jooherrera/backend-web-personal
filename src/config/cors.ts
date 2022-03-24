1fe605b5188afdf53eeff219c0402648e6655d64
Jose Herrera <jooherrera4@gmail.com>
2022-03-24 01:25:52 -0300

    cors modified

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,14 +1,9 @@
 import { Request } from 'express'
+import cors from 'cors'
 import { envVariables } from './constantes'
 
-const allowedOrigins = [envVariables.ALLOWED_ORIGIN]
+const allowedOrigin = envVariables.ALLOWED_ORIGIN
 
-export const options = (req: Request, cb: any) => {
-  let corsOptions = {}
-  if (allowedOrigins.indexOf(req.header('Origin')!) !== -1) {
-    corsOptions = { origin: true }
-  } else {
-    corsOptions = { origin: false }
-  }
-  cb(null, corsOptions)
+export const options: cors.CorsOptions = {
+  origin: allowedOrigin,
 }
